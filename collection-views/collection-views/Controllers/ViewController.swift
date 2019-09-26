46d3301c5d2a0f3fba24b1d9ffe1f623909a46b9
davidlawrencer <davidlrifkin+github@gmail.com>
2019-09-26 12:46:54 -0400

    Adds angelas solution

--- collection-views/collection-views/Controllers/ViewController.swift
+++ collection-views/collection-views/Controllers/ViewController.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var elementCollectionView: UICollectionView!
+    
     var elements = [Element]() {
         didSet {
-            //Do something??
+            elementCollectionView.reloadData()
         }
     }
+
+    //one way to layout
+        func layout() {
+        guard let layout = self.elementCollectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+            return
+        }
+        layout.itemSize = CGSize(width: (self.elementCollectionView.frame.size.width - 20) / 3, height: self.elementCollectionView.frame.size.height / 4)
+        layout.scrollDirection = .horizontal
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loadData()
+        elementCollectionView.dataSource = self
+        layout()
         // Do any additional setup after loading the view.
     }
     
@@ -36,5 +49,20 @@ class ViewController: UIViewController {
     }
 
 
+}
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return elements.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = elementCollectionView.dequeueReusableCell(withReuseIdentifier: "cellOne", for: indexPath) as? ElementCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        let element = elements[indexPath.row]
+        cell.atomicMassLabel.text = "\(element.atomicMass)"
+        cell.nameLabel.text = element.name
+        return cell
+    }
 }
 
---
+++ collection-views/collection-views/Views/ElementCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ElementCollectionViewCell.swift
+//  collection-views
+//
+//  Created by David Rifkin on 9/26/19.
+//  Copyright © 2019 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class ElementCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var atomicMassLabel: UILabel!
+}
